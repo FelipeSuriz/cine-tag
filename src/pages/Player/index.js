259217efd502cdc425514ed13bc1b2de6e0585bc
Felipe Suriz <felipesuriz@hotmail.com>
2023-08-16 10:17:30 -0300

    Criando a Página de erro

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,12 +3,17 @@ import styles from './Player.module.css';
 import videos from 'json/db.json';
 import Titulo from 'components/Titulo';
 import { useParams } from 'react-router-dom';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 export function Player() {
     const parametros = useParams();
     const video = videos.find((video) => {
         return video.id === Number(parametros.id);
     })
+
+    if(!video) {
+        return <NaoEncontrada />
+    }
 
     return (
         <>
